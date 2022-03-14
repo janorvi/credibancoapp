2d0c799909b442265bf4e81ae47b119e9f07c0af
ingjairoorjuela <jairoorjuela@ssintco.com>
2022-03-13 23:51:42 -0500

    creating BottomNavigation and Fragments

--- app/src/main/java/com/example/credibancoapp/MainActivity.kt
+++ app/src/main/java/com/example/credibancoapp/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.credibancoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.credibancoapp.fragment.AnnulmentFragment
+import com.example.credibancoapp.fragment.AprobedTransactionsFragment
+import com.example.credibancoapp.fragment.AuthorizationFragment
+import com.example.credibancoapp.fragment.SearchAprobedTransactionsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val annulmentFragment = AnnulmentFragment()
+    private val aprobedTransactionsFragment = AprobedTransactionsFragment()
+    private val authorizationFragment = AuthorizationFragment()
+    private val searchAprobedTransactionsFragment = SearchAprobedTransactionsFragment()
+    private var bottomNavigationView: BottomNavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(annulmentFragment)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView?.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.item_1 -> replaceFragment(authorizationFragment)
+                R.id.item_2 -> replaceFragment(searchAprobedTransactionsFragment)
+                R.id.item_3 -> replaceFragment(aprobedTransactionsFragment)
+                R.id.item_4 -> replaceFragment(annulmentFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.frame_layout, fragment)
+            transaction.commit();
+        }
     }
 }
